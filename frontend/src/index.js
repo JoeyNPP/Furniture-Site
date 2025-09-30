acbdddeb064dbbb7e6165b006a126ba31e01c9f3
JoeyNPP <natvendors@gmail.com>
2025-09-30 02:10:14 +0000

    Ensured SettingsProvider in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { SettingsProvider } from './settings/SettingsContext';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <SettingsProvider>
-        <App />
-      </SettingsProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter>
+    <SettingsProvider>
+      <App />
+    </SettingsProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
