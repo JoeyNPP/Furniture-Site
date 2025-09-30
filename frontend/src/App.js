5e8c0b075940452b495247894d6031359949224b
JoeyNPP <natvendors@gmail.com>
2025-09-30 02:08:29 +0000

    Added ProtectedRoute to enforce login redirection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from "react-router-dom";
 import Login from "./components/Login";
 import InternalProductList from "./components/InternalProductList";
 import { Box } from "@mui/material";
+
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  const location = useLocation();
+
+  if (!token) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+  return children;
+};
 
 const App = () => {
   return (
@@ -10,8 +20,15 @@ const App = () => {
       <Box sx={{ p: 2 }}>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/products" element={<InternalProductList />} />
-          <Route path="/" element={<InternalProductList />} />
+          <Route
+            path="/products"
+            element={
+              <ProtectedRoute>
+                <InternalProductList />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/" element={<Navigate to="/login" replace />} />
         </Routes>
       </Box>
     </Router>
